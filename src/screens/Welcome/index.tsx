233ac74c621e86c281ecad461ba63d1ba65f4cf5
Isaque Akamine <isaqueakamine@gmail.com>
2024-01-04 07:01:13 -0300

    feat: initial commit with welcome screen

---
+++ src/screens/Welcome/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// import { Container } from './styles';
+
+const Welcome: React.FC = () => {
+  return (
+    <View>
+      <Text>Welcome screen</Text>
+    </View>
+  );
+};
+
+export default Welcome;
